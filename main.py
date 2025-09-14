0d2c9540216e1f60babb5d5fd7925bc8c44aa63b
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-09-14 13:04:49 +0500

    Test

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def recursiveTest():
-    print("Yo gurt")
+# bad_example.py
+
+
+def MyFunc(x, y):  # bad: function name should be snake_case, weird spaces
+    print("Sum is", x + y)  # bad: print usage flagged by T201
+    return x + y  # bad: double space before x+y
+
+
+def another_func():
+    pass  # bad: wrong indentation (2 spaces)
